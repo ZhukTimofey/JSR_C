e142064bf5c5159fbf900d9ab96f8be3ed37a192
Your Name <timofey695@gmail.com>
2021-04-11 19:39:09 +0300

    3

--- app/src/pages/CounterPage/components/Counter/index.js
+++ app/src/pages/CounterPage/components/Counter/index.js
@@ -1,31 +1,44 @@
-import React from 'react';
+import React from "react";
 import PropTypes from "prop-types";
-import {ROUTES} from "../../../../routes/routesNames";
-import {Link} from "react-router-dom";
+import { ROUTES } from "../../../../routes/routesNames";
+import { Link } from "react-router-dom";
 
-import './style.css'
+import "./style.css";
 
-const Counter = ({countValue,parity,handleIncrement,handleDecrement,handleReset,countScreen}) => {
-    return (
-        <div className='counter_wrapper'>
-            <div className={countScreen}> {countValue}</div>
-            <div>{parity}</div>
-            <div className ='buttons_wrapper'>
-                <button className='buttons' onClick={handleIncrement}>+</button>
-                <button className='buttons' onClick={handleReset}>reset</button>
-                <button className='buttons' onClick={handleDecrement}>-</button>
-            </div>
-            <Link to={ROUTES.HOME_PAGE}>
-                <button>Go to home page</button>
-            </Link>
-        </div>
-
-    );
+const Counter = ({
+  countValue,
+  parity,
+  handleIncrement,
+  handleDecrement,
+  handleReset,
+  countScreenClassName,
+}) => {
+  return (
+    <div className="counter_wrapper">
+      <div className={countScreenClassName}> {countValue}</div>
+      <div>{parity}</div>
+      <div className="buttons_wrapper">
+        <button className="buttons" onClick={handleIncrement}>
+          +
+        </button>
+        <button className="buttons" onClick={handleReset}>
+          reset
+        </button>
+        <button className="buttons" onClick={handleDecrement}>
+          -
+        </button>
+      </div>
+      <Link to={ROUTES.HOME_PAGE}>
+        <button>Go to home page</button>
+      </Link>
+    </div>
+  );
 };
-Counter.propTypes ={
-    countValue: PropTypes.number.isRequired,
-    handleIncrement: PropTypes.func.isRequired,
-    handleDecrement: PropTypes.func.isRequired,
-    handleReset: PropTypes.func.isRequired,
-}
+Counter.propTypes = {
+  countValue: PropTypes.number.isRequired,
+  handleIncrement: PropTypes.func.isRequired,
+  handleDecrement: PropTypes.func.isRequired,
+  handleReset: PropTypes.func.isRequired,
+  countScreenClassName: PropTypes.string.isRequired,
+};
 export default Counter;
--- app/src/pages/CounterPage/containers/CounterPageContainer.js
+++ app/src/pages/CounterPage/containers/CounterPageContainer.js
@@ -1,75 +1,67 @@
-import React, {Component} from 'react';
-import Counter from '../components/Counter';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+
+import Counter from "../components/Counter";
+import PropTypes from "prop-types";
 
 class CounterPageContainer extends Component {
-    constructor(props) {
+  constructor(props) {
+    super(props);
+    this.state = {
+      countValue: 0,
+      parity: "even",
+      countScreenClassName: "countScreenR",
+    };
+  }
+  handleIncrement = () => {
+    this.setState((state) => {
+      const countValue = state.countValue + 1;
+      const parity = countValue % 2 == 0 ? "even" : "odd";
+      const countScreenClassName =
+        countValue % 2 == 0 ? "countScreenR" : "countScreenB";
+      return {
+        ...state,
+        countValue,
+        parity,
+        countScreenClassName,
+      };
+    });
+  };
 
-        super(props);
-        this.state ={
-            countValue:0,
-            parity:'even',
-            countScreen:'countScreenR'
-
-
-
-        }
-    }
-    handleIncrement =() =>{
-    this.setState((state)=>{
-        const countValue = state.countValue+1;
-        let parity= ''
-        let countScreen =''
-        if(countValue%2==0) {
-            parity='even'
-            countScreen= 'countScreenR'
-        } else {
-            parity ='odd'
-            countScreen= 'countScreenB'
-        }
+  handleDecrement = () => {
+    this.setState((state) => {
+      const countValue = state.countValue - 1;
+      if (countValue >= 0) {
+        const parity = countValue % 2 == 0 ? "even" : "odd";
+        const countScreenClassName =
+          countValue % 2 == 0 ? "countScreenR" : "countScreenB";
         return {
-            ...state,
-            countValue,
-            parity,
-            countScreen
-        }
-    })
-    }
-
-    handleDecrement =() =>{
-        this.setState((state)=>{
-            let countValue = state.countValue-1;
-            if (countValue<0) {
-                countValue=0
-            }
-            let parity= ''
-            let countScreen ={}
-            if(countValue%2==0) {
-                parity='even'
-                countScreen= 'countScreenR'
-            } else {
-                parity ='odd'
-                countScreen= 'countScreenB'
-            }
-
-            return {
-                ...state,
-                countValue,
-                parity,
-                countScreen
-            }
-        })
-    }
-    handleReset = () =>{
-        this.setState({countValue: 0,parity:'even'})
-        this.setState({countScreen: 'countScreenR'})
-    }
-    render() {
-        return (
-            <Counter countValue={this.state.countValue} parity={this.state.parity} handleIncrement={this.handleIncrement} handleDecrement={this.handleDecrement} handleReset={this.handleReset} countScreen={this.state.countScreen}/>
-        );
-    }
+          ...state,
+          countValue,
+          parity,
+          countScreenClassName,
+        };
+      }
+    });
+  };
+  handleReset = () => {
+    this.setState({
+      countValue: 0,
+      parity: "even",
+      countScreenClassName: "countScreenR",
+    });
+  };
+  render() {
+    return (
+      <Counter
+        countValue={this.state.countValue}
+        parity={this.state.parity}
+        handleIncrement={this.handleIncrement}
+        handleDecrement={this.handleDecrement}
+        handleReset={this.handleReset}
+        countScreenClassName={this.state.countScreenClassName}
+      />
+    );
+  }
 }
-
 
 export default CounterPageContainer;
